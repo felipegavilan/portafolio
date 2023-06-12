250ba836e611a9d7a9f36eff03e255d061c3efe3
felipegavilan <felipegaviilan@gmail.com>
2023-06-11 21:00:20 -0300

    First commit - portfolio in progress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Nav from "./components/nav/nav";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main>
+      <Nav/>
+      <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
