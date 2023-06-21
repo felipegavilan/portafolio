6c304a87a3f41f1aa22c0eb13af0ca05f2b4743a
felipegavilan <felipegaviilan@gmail.com>
2023-06-20 23:17:36 -0300

    contacto

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,12 @@ import Projects from "./components/projects/projects";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
-  const handlerDownload = () =>{
-    const link = document.createElement('a');
-    link.href = 'felipeGavilanCV.pdf'
-    link.download = 'FelipeGavilanCV'
-    link.click()
-  }
+  const handlerDownload = () => {
+    const link = document.createElement("a");
+    link.href = "felipeGavilanCV.pdf";
+    link.download = "FelipeGavilanCV";
+    link.click();
+  };
   return (
     <div className="m-4">
       <header className="flex flex-1 items-center justify-center">
@@ -26,15 +25,15 @@ export default function Home() {
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300 m-2 text-justify animate-fade-right animate-once animate-duration-1000  animate-ease-in-out">
             Soy un apasionado desarrollador web Full Stack Junior de 24 años,
-            proveniente de Argentina. Estoy buscando nuevos
-            desafíos y oportunidades laborales que me permitan seguir
-            aprendiendo y creciendo en el desarrollo de aplicaciones. Poseo una
-            sólida base de conocimientos y una capacidad excepcional para
-            aprender rápidamente. Siempre estoy dispuesto a enfrentar nuevos
-            retos y continuar mi crecimiento en el fascinante mundo de la
-            tecnología. Mi dedicación, pasión y habilidades destacadas me
-            impulsan a seguir aprendiendo y desarrollándome constantemente en el
-            campo de las tecnologías de la información.
+            proveniente de Argentina. Estoy buscando nuevos desafíos y
+            oportunidades laborales que me permitan seguir aprendiendo y
+            creciendo en el desarrollo de aplicaciones. Poseo una sólida base de
+            conocimientos y una capacidad excepcional para aprender rápidamente.
+            Siempre estoy dispuesto a enfrentar nuevos retos y continuar mi
+            crecimiento en el fascinante mundo de la tecnología. Mi dedicación,
+            pasión y habilidades destacadas me impulsan a seguir aprendiendo y
+            desarrollándome constantemente en el campo de las tecnologías de la
+            información.
           </p>
         </section>
       </header>
@@ -53,16 +52,19 @@ export default function Home() {
       <Skills />
       <div className="flex justify-center mt-8">
         <div className="flex w-44 animate-fade-up animate-once animate-duration-1000 animate-delay-[10ms] animate-ease-in-out">
-          <button 
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          onClick={handlerDownload}
+          <button
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={handlerDownload}
           >
             Descargar CV
           </button>
         </div>
       </div>
-      <div id="projectos" className="flex justify-center mt-8 animate-jump-in animate-once animate-duration-1000 animate-delay-[10ms] animate-ease-in-out">
-        <Projects/>
+      <div
+        id="projectos"
+        className="flex justify-center mt-8 animate-jump-in animate-once animate-duration-1000 animate-delay-[10ms] animate-ease-in-out"
+      >
+        <Projects />
       </div>
     </div>
   );
